a481eca22d12709276be82e44e6f706901e39c16
Harsh Joshi <harshjoshi@Harshs-MacBook-Pro.local>
2024-12-07 13:12:01 +0530

    Update the Cron logic

--- Cron.py
+++ Cron.py
@@ -1,17 +1,23 @@
 from celery import Celery
-from GeoJson import process_geojson 
+from GeoJson import process_geojson
 
 app = Celery('geojson_task', broker='redis://localhost:6379/0')
 
 # Define the task
 @app.task
 def run_geojson_task():
-    process_geojson()  
+    process_geojson()
 
-# To run the task immediately on deployment and seeting up daily cron job
+# To run the task immediately on deployment and then every 10 seconds
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-
+    # Run once immediately after deployment
     run_geojson_task.apply_async()
+    # Schedule to run every 24 hours (86400 seconds) or once a day at a specific time
     
-    sender.add_periodic_task(86400.0, run_geojson_task.s(), name='Run GeoJSON task every 24 hours')
+    sender.add_periodic_task(
+        86400.0,  # Run every 24 hours (86400 seconds)
+        run_geojson_task.s(),
+        name='Run GeoJSON task every 24 hours'
+    )
+
